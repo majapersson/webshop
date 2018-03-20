44c58b5f76dac1ee8d302dcda1554f415c49580a
Maja Persson <persson.maja@gmail.com>
2018-03-20 21:37:22 +0100

    Started working on checkout

---
+++ SnackShop.Core/Models/OrderModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnackShop.Core.Models
+{
+    public class OrderModel
+    {
+        public string Id;
+        public CartModel Cart;
+        public string Name;
+        public string Address;
+        public string Email;
+        public string Phone;
+    }
+}
---
+++ SnackShop/Controllers/CheckoutController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using SnackShop.Core.Models;
+using SnackShop.Core.Services;
+using SnackShop.Core.Repositories;
+
+namespace SnackShop.Controllers
+{
+    public class CheckoutController : Controller
+    {
+        private CartService CartService;
+        private CartModel Cart;
+
+        public CheckoutController(IConfiguration configuration)
+        {
+            this.CartService = new CartService(
+                new CartRepository(
+                    configuration.GetConnectionString("ConnectionString")));
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var cookie = Request.Cookies["CartID"];
+            this.Cart = this.CartService.GetCart(cookie);
+
+            return View(this.Cart);
+        }
+
+        [HttpPost]
+        public IActionResult Index(OrderModel order)
+        {
+            return View();
+        }
+    }
+}
